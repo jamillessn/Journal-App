167dfaee1441c3164aca08f0d8cbc0e5c58eb002
jamillessn <jamillerosesison1@gmail.com>
2024-03-13 16:59:58 +0800

    style: add svgs, tailwind config, index, today, and devise forms styles

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,4 +1,5 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
   content: [
@@ -7,7 +8,68 @@ module.exports = {
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}'
   ],
+  variants: {
+    extend: {
+      backgroundColor: ['responsive', 'hover', 'focus', 'dark', 'dark:hover'],
+      textColor: ['responsive', 'hover', 'focus', 'dark', 'dark:hover'],
+    },
+  },
   theme: {
+    colors: {
+      cyan: '#9cdbff',
+      white: '#ffffff',
+      purple: '#3f3cbb',
+      midnight: '#121063',
+      metal: '#565584',
+      tahiti: '#3ab7bf',
+      silver: '#ecebff',
+      'bubble-gum': '#ff77e9',
+      red: {
+        50: '#FEF3F3',
+        100: '#FEE2E2',
+        200: '#FFCBCA',
+        300: '#FCA5A5',
+        400: '#F97170',
+        500: '#EE4445',
+        600: '#DC2727',
+        700: '#B91C1C',
+        800: '#991A1A',
+        900: '#7E1C1D',
+      },
+      teal: {
+        50: '#F0FDFA',
+        100: '#CDFBF0',
+        200: '#98F7E4',
+        300: '#5FEBD4',
+        400: '#2CD5BF',
+        500: '#15B9A6',
+        600: '#0D9488',
+        700: '#0F766E',
+        800: '#105F59',
+        900: '#124F4A',
+      },
+      bermuda: '#78dcca',
+      primary: colors.blue,
+      brown: {
+        50: '#fdf8f6',
+        100: '#f2e8e5',
+        200: '#eaddd7',
+        300: '#e0cec7',
+        400: '#d2bab0',
+        500: '#bfa094',
+        600: '#a18072',
+        700: '#977669',
+        800: '#846358',
+        900: '#43302b',
+      },
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
@@ -19,53 +81,6 @@ module.exports = {
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
-  ]
-}
-
-tailwind.config = {
-  darkMode: 'class',
-  theme: {
-    extend: {
-      colors: {
-        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
-      }
-    },
-    fontFamily: {
-      'body': [
-    'Inter', 
-    'ui-sans-serif', 
-    'system-ui', 
-    '-apple-system', 
-    'system-ui', 
-    'Segoe UI', 
-    'Roboto', 
-    'Helvetica Neue', 
-    'Arial', 
-    'Noto Sans', 
-    'sans-serif', 
-    'Apple Color Emoji', 
-    'Segoe UI Emoji', 
-    'Segoe UI Symbol', 
-    'Noto Color Emoji'
   ],
-      'sans': [
-    'Inter', 
-    'ui-sans-serif', 
-    'system-ui', 
-    '-apple-system', 
-    'system-ui', 
-    'Segoe UI', 
-    'Roboto', 
-    'Helvetica Neue', 
-    'Arial', 
-    'Noto Sans', 
-    'sans-serif', 
-    'Apple Color Emoji', 
-    'Segoe UI Emoji', 
-    'Segoe UI Symbol', 
-    'Noto Color Emoji'
-  ]
-    }
-  }
-}
-
+  darkMode: 'class', // Enable dark mode
+};
